c382140fb8d886ffd38ccad5220404fc4ff47267
Tony Vitale <vitale13@gmail.com>
2023-02-13 20:50:54 -0500

    wip

---
+++ pkg/astraios/astraios.go
@@ -0,0 +1,9 @@
+// Package astraios provides a way to take photos with a connected camera.
+package astraios
+
+import "io"
+
+// Camera is the interface that provides a way to take pictures via a connected camera.
+type Camera interface {
+	TakePhoto() (io.Reader, error)
+}
